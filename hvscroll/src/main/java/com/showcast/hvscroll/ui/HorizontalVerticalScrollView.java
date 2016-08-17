91ce970bc868c148baaa6291bd3244ea9a101c40
henry <henry@bestapp.us>
2016-08-17 17:22:56 +0800

    创建horizontalVerticalScrollView

---
+++ hvscroll/src/main/java/com/showcast/hvscroll/ui/HorizontalVerticalScrollView.java
@@ -0,0 +1,44 @@
+package com.showcast.hvscroll.ui;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
+import android.view.View;
+
+import com.showcast.hvscroll.draw.HorizontalVerticalScrollDraw;
+import com.showcast.hvscroll.entity.AbsTableEntity;
+import com.showcast.hvscroll.params.GlobalParams;
+import com.showcast.hvscroll.params.TableParams;
+
+/**
+ * Created by taro on 16/8/17.
+ */
+public class HorizontalVerticalScrollView extends View {
+    private HorizontalVerticalScrollDraw mHvDraw = new HorizontalVerticalScrollDraw(this);
+
+    public HorizontalVerticalScrollView(Context context) {
+        super(context);
+        this.initial();
+    }
+
+    public HorizontalVerticalScrollView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        this.initial();
+    }
+
+    public HorizontalVerticalScrollView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        this.initial();
+    }
+
+    private void initial() {
+        mHvDraw.setParams(new GlobalParams(), new TableParams());
+        mHvDraw.setTable(AbsTableEntity.getExampleTable());
+    }
+
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        mHvDraw.drawCanvas(canvas);
+    }
+}
