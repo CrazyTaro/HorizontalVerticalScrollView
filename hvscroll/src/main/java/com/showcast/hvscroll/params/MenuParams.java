400bfcb4f9830f145314706312981dc6cbe688bc
henry <henry@bestapp.us>
2016-08-19 18:20:53 +0800

    新增菜单参数

---
+++ hvscroll/src/main/java/com/showcast/hvscroll/params/MenuParams.java
@@ -0,0 +1,20 @@
+package com.showcast.hvscroll.params;
+
+import com.showcast.hvscroll.draw.MenuDrawStyle;
+
+/**
+ * Created by taro on 16/8/19.
+ */
+public class MenuParams extends BaseParams {
+    private MenuDrawStyle mRowMenuStyle;
+    private MenuDrawStyle mColumnMenuStyle;
+
+    public MenuParams() {
+    }
+
+    public MenuParams(int width, int height) {
+        super(width, height);
+    }
+
+
+}
