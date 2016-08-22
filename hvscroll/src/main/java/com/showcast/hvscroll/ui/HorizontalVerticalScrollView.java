8aaa7d0712e13cd7d8c96620e3b160f8ac8a6ede
henry <henry@bestapp.us>
2016-08-22 15:58:42 +0800

    更新绘制对象的参数设置

--- hvscroll/src/main/java/com/showcast/hvscroll/ui/HorizontalVerticalScrollView.java
+++ hvscroll/src/main/java/com/showcast/hvscroll/ui/HorizontalVerticalScrollView.java
@@ -7,7 +7,10 @@ import android.view.View;
 
 import com.showcast.hvscroll.draw.HorizontalVerticalScrollDraw;
 import com.showcast.hvscroll.entity.AbsTableEntity;
+import com.showcast.hvscroll.params.CellParams;
+import com.showcast.hvscroll.params.Constant;
 import com.showcast.hvscroll.params.GlobalParams;
+import com.showcast.hvscroll.params.MenuParams;
 
 /**
  * Created by taro on 16/8/17.
@@ -31,7 +34,11 @@ public class HorizontalVerticalScrollView extends View {
     }
 
     private void initial() {
-        mHvDraw.setParams(new GlobalParams());
+        MenuParams menuParams = new MenuParams();
+        menuParams.setIsDrawRowMenu(true);
+        menuParams.addFrozenMenuIndex(0, Constant.MENU_ROW);
+        menuParams.getMenuSetting(Constant.MENU_ROW).setMenuFrozen(false, true);
+        mHvDraw.setParams(new GlobalParams(), menuParams, new CellParams());
         mHvDraw.setTable(AbsTableEntity.getExampleTable());
     }
 
